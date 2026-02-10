8b1461684e4f0722c38cb4fe8cdecf68c875cf08
doi02 <dohee0807@gmail.com>
2026-02-10 14:48:20 +0900

    수정, 삭제시 작성자 인증관련 에러코드, 로직 수정2

---
+++ ena/src/main/java/io/github/doi02/ena/dto/post/PostDeleteRequest.java
@@ -0,0 +1,10 @@
+package io.github.doi02.ena.dto.post;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostDeleteRequest {
+    private Long userId;
+}
