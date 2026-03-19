e8633a562ab544e8319962f5d28a0dc737f9562b
doi02 <dohee0807@gmail.com>
2026-03-20 04:31:52 +0900

    헬스체크 api swagger 설명 추가

--- ena/src/main/java/io/github/doi02/ena/controller/HealthCheckController.java
+++ ena/src/main/java/io/github/doi02/ena/controller/HealthCheckController.java
@@ -1,5 +1,8 @@
 package io.github.doi02.ena.controller;
 
+import io.github.doi02.ena.common.config.LoginUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+    @Operation(summary = "서버 헬스체크", description = "서버 상태가 온전한지 확인합니다.")
     @GetMapping("/api/")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("ok");
